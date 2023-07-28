aea5136d24888c601d3264fd4287aea043ba89fd
Daniel Middleton <middleton.daniel@gmail.com>
2023-07-28 16:32:04 -0500

    Basic Chat UI (#1)

--- src/components/chat/Chat.tsx
+++ src/components/chat/Chat.tsx
@@ -1,10 +1,12 @@
-import { Box, Button, TextField, Typography } from "@suid/material";
+import { Button, TextField } from "@kobalte/core";
 import { For, createEffect, createSignal } from "solid-js";
+import css from "./Chat.module.css";
 
 const messageFieldName = "newMessage";
 const birdMessage = {
-  text: "I'm Larry A. Bird",
-  user: "bird",
+  text: "I'm a bird!",
+  userType: "bird",
+  username: "Larry A. Bird",
 };
 
 type Message = typeof birdMessage;
@@ -12,7 +14,8 @@ type Message = typeof birdMessage;
 export function Chat() {
   const [messages, setMessages] = createSignal<Message[]>([birdMessage]);
   createEffect(() => {
-    if (messages()[messages().length - 1].user === "human") {
+    // Trigger a reply from the bird when the last message is from the human
+    if (messages()[messages().length - 1].userType === "human") {
       setMessages((messages) => [...messages, birdMessage]);
     }
   });
@@ -20,44 +23,60 @@ export function Chat() {
   return (
     <div>
       <For each={messages()}>
-        {({ user, text }) => (
-          <Box>
-            <Typography>{user}</Typography>
-            <Typography>{text}</Typography>
-          </Box>
+        {({ userType, text, username }) => (
+          <div
+            classList={{
+              [css.message]: true,
+              [css.message_bird]: userType === "bird",
+              [css.message_human]: userType === "human",
+            }}
+          >
+            <div class={css.message_user}>{username}</div>
+            <div class={css.message_text}>{text}</div>
+          </div>
         )}
       </For>
       <form
+        class={css.form}
         onSubmit={(e) => {
           e.preventDefault();
+          if (!e.currentTarget[messageFieldName].value) {
+            return;
+          }
+
           setMessages((messages) => [
             ...messages,
             {
               text: e.currentTarget[messageFieldName].value,
-              user: "human",
+              userType: "human",
+              username: "Daniel",
             },
           ]);
         }}
       >
-        <TextField
-          multiline
-          onKeyDown={(e) => {
-            if (e.key === "Enter" && !e.ctrlKey) {
-              e.preventDefault();
-              e.target.form.dispatchEvent(
-                new Event("submit", { cancelable: true })
-              );
-              e.target.value = "";
-            }
+        <TextField.Root class={css.textRoot}>
+          <TextField.TextArea
+            placeholder="Send a message"
+            class={css.textarea}
+            name={messageFieldName}
+            onKeyDown={(e) => {
+              if (e.key === "Enter" && !e.ctrlKey) {
+                e.preventDefault();
+                e.currentTarget.form?.dispatchEvent(
+                  new Event("submit", { cancelable: true })
+                );
+                e.currentTarget.value = "";
+              }
 
-            if (e.key === "Enter" && e.ctrlKey) {
-              e.target.value += "\n";
-            }
-          }}
-          name={messageFieldName}
-          placeholder="What do you want to tell the bird?"
-        ></TextField>
-        <Button type="submit">Send</Button>
+              if (e.key === "Enter" && e.ctrlKey) {
+                e.currentTarget.value += "\n";
+              }
+            }}
+          />
+        </TextField.Root>
+        <Button.Root type="submit" class={css.button}>
+          Send
+        </Button.Root>
       </form>
     </div>
   );
