80c40fc549708ace12dd2f2f95bfc4127a354e70
Daniel Middleton <middleton.daniel@gmail.com>
2023-07-28 18:08:22 -0500

    Introduce Vercel support (#3)

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import solid from "solid-start/vite";
 import { defineConfig } from "vite";
 import eslint from "vite-plugin-eslint";
+import vercel from "solid-start-vercel";
 
 export default defineConfig({
-  plugins: [solid(), eslint()],
+  plugins: [solid({ adapter: vercel({}) }), eslint()],
   ssr: {
     noExternal: ["@kobalte/core"],
   },
