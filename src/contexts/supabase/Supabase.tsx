34a8c15f95d90a698632322e5a508b878df444c7
Daniel Middleton <middleton.daniel@gmail.com>
2023-07-29 23:42:59 -0500

    Disable some auth flags when on server

--- src/contexts/supabase/Supabase.tsx
+++ src/contexts/supabase/Supabase.tsx
@@ -1,9 +1,17 @@
 import { createContext } from "solid-js";
 import { createClient } from "@supabase/supabase-js";
+import { isServer } from "solid-js/web";
 
 export const supabase = createClient(
   import.meta.env.VITE_SUPABASE_API_URL,
   import.meta.env.VITE_SUPABASE_API_KEY,
+  {
+    auth: {
+      autoRefreshToken: !isServer,
+      persistSession: !isServer,
+      detectSessionInUrl: !isServer,
+    },
+  },
 );
 
 export const SupabaseContext = createContext(supabase);
